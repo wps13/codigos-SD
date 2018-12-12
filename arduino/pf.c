3cff252475225837901afde5555d2ba089691051
wps13 <willanepaiva@hotmail.com>
2018-12-12 09:17:34 -0300

    Fix code

--- arduino/pf.c
+++ arduino/pf.c
@@ -8,7 +8,7 @@ uint8_t ldrConversor();
 void setup(){
 	//PB4(D12) = MISO, envia dados
 	//PB3(D11) = MOSI, recebe dados
-	DDRB = 0b00010000;
+	DDRB = 0b01010000;
 	SPCR = 0b01000001;
 	ADMUX = 0b01000000;  //seleciona AVCC como referencia
     ADCSRA = 0b10000111; //habilita adc e fator de divisao da f = 128
@@ -22,6 +22,7 @@ void main(){
 		//Aguarda fim do envio
 		while(!(SPSR & (1<<SPIF)));
 		recebido = SPDR;
+		PINB6 = recebido; 
 	}
 }
 
